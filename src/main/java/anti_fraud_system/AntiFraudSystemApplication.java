2304ec5edf225e1ca28fbf11d1ecdf87252b9e43
Stefan Bock <143817813+stiebo@users.noreply.github.com>
2024-09-30 15:25:37 +0200

    Initial commit

---
+++ src/main/java/anti_fraud_system/AntiFraudSystemApplication.java
@@ -0,0 +1,13 @@
+package anti_fraud_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiFraudSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntiFraudSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/anti_fraud_system/AntiFraudSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package anti_fraud_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiFraudSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
