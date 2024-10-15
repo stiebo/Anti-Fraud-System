7e2c340f6d2fef673c0a44f9d351639bc80c45cc
Stefan Bock <143817813+stiebo@users.noreply.github.com>
2024-10-15 21:44:37 +0200

    fix: minor fixes

--- src/main/java/antifraud/controller/UserController.java
+++ src/main/java/antifraud/controller/UserController.java
@@ -1,6 +1,10 @@
 package antifraud.controller;
 
 import antifraud.dto.*;
+import antifraud.exception.RoleAlreadyProvided;
+import antifraud.exception.UnableToLockAdminException;
+import antifraud.exception.UserExistsException;
+import antifraud.exception.UserNotFoundException;
 import antifraud.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -22,32 +27,36 @@ public class UserController {
 
     @PostMapping("/user")
     @ResponseStatus(HttpStatus.CREATED)
-    public UserDto createUser(@Valid @RequestBody NewUserDto newUserDto) {
+    public UserDto createUser(@Valid @RequestBody NewUserDto newUserDto)
+            throws UserExistsException {
         return service.createUser(newUserDto);
     }
 
     @GetMapping("/list")
-    public UserDto[] getUsers () {
+    public List<UserDto> getUsers() {
         return service.listUsers();
     }
 
     @DeleteMapping("/user/{username}")
-    public DeleteUserDto deleteUser(@PathVariable("username") String username) {
+    public DeleteUserDto deleteUser(@PathVariable("username") String username)
+            throws UserNotFoundException {
         service.deleteUser(username);
         return new DeleteUserDto(username, "Deleted successfully!");
     }
 
     @PutMapping("/role")
-    public UserDto changeRole(@Valid @RequestBody ChangeRoleDto changeRoleDto) {
+    public UserDto changeRole(@Valid @RequestBody ChangeRoleDto changeRoleDto)
+            throws UserNotFoundException, RoleAlreadyProvided {
         return service.changeRole(changeRoleDto);
     }
 
     @PutMapping("/access")
-    public Map<String,String> changeAccess(@Valid @RequestBody ChangeAccessDto changeAccessDto) {
+    public Map<String, String> changeAccess(@Valid @RequestBody ChangeAccessDto changeAccessDto)
+            throws UserNotFoundException, UnableToLockAdminException {
         service.changeAccess(changeAccessDto);
         return Collections.singletonMap("status",
                 String.format("User %s %s!", changeAccessDto.username(),
                         changeAccessDto.operation().equals("LOCK") ?
-                        "locked" : "unlocked"));
+                                "locked" : "unlocked"));
     }
 }
