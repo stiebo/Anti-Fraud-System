9e92619268968d93ce77f649283b0acd3a869f50
Stefan Bock <143817813+stiebo@users.noreply.github.com>
2024-11-10 12:56:16 +0100

    add custom error.html

---
+++ src/main/java/antifraud/controller/CustomErrorController.java
@@ -0,0 +1,14 @@
+package antifraud.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "forward:/error.html";  // Forward directly to the static error.html page
+    }
+}
