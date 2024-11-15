ae5ffdc11bd41b8abf8e68270b8b5367dd636a09
Stefan Bock <143817813+stiebo@users.noreply.github.com>
2024-11-15 15:59:23 +0100

    remove unused import

--- src/test/java/antifraud/repository/UserRepositoryTest.java
+++ src/test/java/antifraud/repository/UserRepositoryTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static antifraud.TestDataUtil.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import antifraud.domain.Role;
 import antifraud.domain.User;
 import antifraud.security.RoleLoader;
 import org.junit.jupiter.api.BeforeEach;
